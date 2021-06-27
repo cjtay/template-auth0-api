ac85fb3cd465783550332839f7dc17a9cf3a5c7d
Tay CJ <cjtay@outlook.sg>
2021-06-27 20:02:08 +0800

    added requireAuth

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,23 @@
 const express = require('express');
+require('dotenv').config();
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
 const router = express.Router();
 
 const controllers = require('../controllers/users');
+
+const requireAuth = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-kcs-n29r.au.auth0.com/.well-known/jwks.json',
+    }),
+    audience: 'https://digitalfunnel/',
+    issuer: 'https://dev-kcs-n29r.au.auth0.com/',
+    algorithms: ['RS256'],
+});
 
 router.get('/', (req, res) => {
     res.send('hello from routes folder');
@@ -11,7 +27,7 @@ router.get('/other', (req, res) => {
     res.send('hello from other route!');
 });
 
-router.get('/user', controllers.getUser);
+router.get('/user', requireAuth, controllers.getUser);
 router.post('/adduser', controllers.addUser);
 
 module.exports = router;
